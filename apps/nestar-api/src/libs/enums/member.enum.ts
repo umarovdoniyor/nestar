2d5d030a7e7a7ccdb29cbfef0eeb927ad97fa1f9
Danny <mr.danny951008@gmail.com>
2025-12-25 17:53:37 +0900

    feat: create Schema Models and project enums

---
+++ apps/nestar-api/src/libs/enums/member.enum.ts
@@ -0,0 +1,22 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum MemberType {
+	USER = 'USER',
+	ADMIN = 'ADMIN',
+	AGENT = 'AGENT',
+}
+registerEnumType(MemberType, { name: 'MemberType' });
+
+export enum MemberStatus {
+	ACTIVE = 'ACTIVE',
+	BLOCK = 'BLOCK',
+	DELETE = 'DELETE',
+}
+registerEnumType(MemberStatus, { name: 'MemberStatus' });
+
+export enum MemberAuthType {
+	EMAIL = 'EMAIL',
+	PHONE = 'PHONE',
+	TELEGRAM = 'TELEGRAM',
+}
+registerEnumType(MemberAuthType, { name: 'MemberAuthType' });
