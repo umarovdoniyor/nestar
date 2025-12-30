63e653f0288e49d75a78f5eb876c6f75b4620d06
Danny <mr.danny951008@gmail.com>
2025-12-31 04:44:41 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,14 +10,12 @@ export class MemberResolver {
   @Mutation(() => Member)
   public async signup(@Args('input') input: MemberInput): Promise<Member> {
     console.log('Mutation:signup');
-    console.log('input: ', input);
     return await this.memberService.signup(input);
   }
 
   @Mutation(() => Member)
   public async login(@Args('input') input: LoginInput): Promise<Member> {
     console.log('Mutation:login');
-    console.log('input: ', input);
     return await this.memberService.login(input);
   }
 
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -37,7 +37,6 @@ export class MemberService {
     }
 
     // TODO: Compare passwords
-    console.log('response: ', response);
     const isMatch = memberPassword === response.memberPassword;
     if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
