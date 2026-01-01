f39f5974cf1889f218634fe4a6501d0e6116adcb
Danny <mr.danny951008@gmail.com>
2026-01-01 19:18:06 +0900

    fix: createion and verification jwt

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -20,6 +20,8 @@ export class MemberService {
     try {
       const result = await this.memberModel.create(input);
       // TODO: Authentication via TOKEN
+      result.accessToken = await this.authService.createToken(result);
+
       return result;
     } catch (err) {
       console.log('Error, Service.model: ', err.message);
@@ -43,7 +45,7 @@ export class MemberService {
     // TODO: Compare passwords
     const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
     if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
-
+    response.accessToken = await this.authService.createToken(response);
     return response;
   }
 
--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -4,77 +4,80 @@ import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enu
 
 @ObjectType()
 export class Member {
-	@Field(() => String)
-	_id: ObjectId;
+  @Field(() => String)
+  _id: ObjectId;
 
-	@Field(() => MemberType)
-	memberType: MemberType;
+  @Field(() => MemberType)
+  memberType: MemberType;
 
-	@Field(() => MemberStatus)
-	memberStatus: MemberStatus;
+  @Field(() => MemberStatus)
+  memberStatus: MemberStatus;
 
-	@Field(() => MemberAuthType)
-	memberAuthType: MemberAuthType;
+  @Field(() => MemberAuthType)
+  memberAuthType: MemberAuthType;
 
-	@Field(() => String)
-	memberPhone: string;
+  @Field(() => String)
+  memberPhone: string;
 
-	@Field(() => String)
-	memberNick: string;
+  @Field(() => String)
+  memberNick: string;
 
-	memberPassword?: string;
+  memberPassword?: string;
 
-	@Field(() => String, { nullable: true })
-	memberFullName?: string;
+  @Field(() => String, { nullable: true })
+  memberFullName?: string;
 
-	@Field(() => String)
-	memberImage: string;
+  @Field(() => String)
+  memberImage: string;
 
-	@Field(() => String, { nullable: true })
-	memberAddress?: string;
+  @Field(() => String, { nullable: true })
+  memberAddress?: string;
 
-	@Field(() => String, { nullable: true })
-	memberDesc?: string;
+  @Field(() => String, { nullable: true })
+  memberDesc?: string;
 
-	@Field(() => Int)
-	memberProperties: number;
+  @Field(() => Int)
+  memberProperties: number;
 
-	@Field(() => Int)
-	memberArticles: number;
+  @Field(() => Int)
+  memberArticles: number;
 
-	@Field(() => Int)
-	memberFollowers: number;
+  @Field(() => Int)
+  memberFollowers: number;
 
-	@Field(() => Int)
-	memberFollowing: number;
+  @Field(() => Int)
+  memberFollowing: number;
 
-	@Field(() => Int)
-	memberPoints: number;
+  @Field(() => Int)
+  memberPoints: number;
 
-	@Field(() => Int)
-	memberLikes: number;
+  @Field(() => Int)
+  memberLikes: number;
 
-	@Field(() => Int)
-	memberViews: number;
+  @Field(() => Int)
+  memberViews: number;
 
-	@Field(() => Int)
-	memberComments: number;
+  @Field(() => Int)
+  memberComments: number;
 
-	@Field(() => Int)
-	memberRank: number;
+  @Field(() => Int)
+  memberRank: number;
 
-	@Field(() => Int)
-	memberWarnings: number;
+  @Field(() => Int)
+  memberWarnings: number;
 
-	@Field(() => Int)
-	memberBlocks: number;
+  @Field(() => Int)
+  memberBlocks: number;
 
-	@Field(() => Date, { nullable: true })
-	deletedAt?: Date;
+  @Field(() => Date, { nullable: true })
+  deletedAt?: Date;
 
-	@Field(() => Date)
-	createdAt: Date;
+  @Field(() => Date)
+  createdAt: Date;
 
-	@Field(() => Date)
-	updatedAt: Date;
+  @Field(() => Date)
+  updatedAt: Date;
+
+  @Field(() => String, { nullable: true })
+  accessToken?: string;
 }
