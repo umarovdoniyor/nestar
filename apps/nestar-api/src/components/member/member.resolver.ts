33a8981c52e076eba9b59981d876740d1b644f16
Danny <mr.danny951008@gmail.com>
2026-01-02 23:43:12 +0900

    fix: modify updateMember GraphQL Api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -24,13 +25,6 @@ export class MemberResolver {
   public async login(@Args('input') input: LoginInput): Promise<Member> {
     console.log('Mutation:login');
     return await this.memberService.login(input);
-  }
-
-  @UseGuards(AuthGuard)
-  @Mutation(() => String)
-  public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-    console.log('Mutation:updateMember');
-    return await this.memberService.updateMember();
   }
 
   // Testing purposes
@@ -49,6 +43,17 @@ export class MemberResolver {
     console.log('Query:checkAuthRoles');
 
     return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => Member)
+  public async updateMember(
+    @Args('input') input: MemberUpdate,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Member> {
+    console.log('Mutation:updateMember');
+    delete input._id;
+    return await this.memberService.updateMember(memberId, input);
   }
 
   @Query(() => String)
