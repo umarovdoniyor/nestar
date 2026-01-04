3183a00a846b0f4afb42b0381504531f2d5e772d
Danny <mr.danny951008@gmail.com>
2026-01-05 01:56:43 +0900

    feat: develop getAllMembersByAdmin and updateMemberByAdmin GraphQL apis

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
@@ -72,7 +72,7 @@ export class MemberResolver {
   @Query(() => Members)
   public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('id') memberId: ObjectId): Promise<Members> {
     console.log('Query: getAgents');
-    return this.memberService.getAgents(memberId, input);
+    return await this.memberService.getAgents(memberId, input);
   }
 
   /** ADMIN **/
@@ -80,15 +80,18 @@ export class MemberResolver {
   // Authorization: ADMIN
   @Roles(MemberType.ADMIN)
   @UseGuards(RolesGuard)
-  @Mutation(() => String)
-  public async getAllMembersByAdmin(@AuthMember() authMember: Member): Promise<string> {
-    console.log('authMember.memberType:', authMember.memberType);
-    return this.memberService.getAllMembersByAdmin();
+  @Query(() => Members)
+  public async getAllMembersByAdmin(@Args('input') input: MembersInquiry): Promise<Members> {
+    console.log('Query: getAllMembersByAdmin');
+    return await this.memberService.getAllMembersByAdmin(input);
   }
 
   // Authorization: ADMIN
-  @Mutation(() => String)
-  public async updateMemberByAdmin(): Promise<string> {
-    return this.memberService.updateMemberByAdmin();
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
+  @Mutation(() => Member)
+  public async updateMemberByAdmin(@Args('input') input: MemberUpdate): Promise<Member> {
+    console.log('Mutation: updateMemberByAdmin');
+    return await this.memberService.updateMemberByAdmin(input);
   }
 }
