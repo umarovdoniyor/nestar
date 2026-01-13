adb79a0c9af9a59ae39eec92d8e2b3fb56ed0163
Danny <mr.danny951008@gmail.com>
2026-01-13 17:33:49 +0900

    feat: configure boardArticle module and integrate boardArticle DTO

--- apps/nestar-api/src/libs/enums/board-article.enum.ts
+++ apps/nestar-api/src/libs/enums/board-article.enum.ts
@@ -1,19 +1,19 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 export enum BoardArticleCategory {
-	FREE = 'FREE',
-	RECOMMEND = 'RECOMMEND',
-	NEWS = 'NEWS',
-	HUMOR = 'HUMOR',
+  FREE = 'FREE',
+  RECOMMEND = 'RECOMMEND',
+  NEWS = 'NEWS',
+  HUMOR = 'HUMOR',
 }
 registerEnumType(BoardArticleCategory, {
-	name: 'BoardArticleCategory',
+  name: 'BoardArticleCategory',
 });
 
 export enum BoardArticleStatus {
-	ACTIVE = 'ACTIVE',
-	DELETE = 'DELETE',
+  ACTIVE = 'ACTIVE',
+  DELETE = 'DELETE',
 }
 registerEnumType(BoardArticleStatus, {
-	name: 'BoardArticleStatus',
+  name: 'BoardArticleStatus',
 });
--- apps/nestar-api/src/schemas/BoardArticle.model.ts
+++ apps/nestar-api/src/schemas/BoardArticle.model.ts
@@ -2,55 +2,55 @@ import { Schema } from 'mongoose';
 import { BoardArticleCategory, BoardArticleStatus } from '../libs/enums/board-article.enum';
 
 const BoardArticleSchema = new Schema(
-	{
-		articleCategory: {
-			type: String,
-			enum: BoardArticleCategory,
-			required: true,
-		},
+  {
+    articleCategory: {
+      type: String,
+      enum: BoardArticleCategory,
+      required: true,
+    },
 
-		articleStatus: {
-			type: String,
-			enum: BoardArticleStatus,
-			default: BoardArticleStatus.ACTIVE,
-		},
+    articleStatus: {
+      type: String,
+      enum: BoardArticleStatus,
+      default: BoardArticleStatus.ACTIVE,
+    },
 
-		articleTitle: {
-			type: String,
-			required: true,
-		},
+    articleTitle: {
+      type: String,
+      required: true,
+    },
 
-		articleContent: {
-			type: String,
-			required: true,
-		},
+    articleContent: {
+      type: String,
+      required: true,
+    },
 
-		articleImage: {
-			type: String,
-		},
+    articleImage: {
+      type: String,
+    },
 
-		articleLikes: {
-			type: Number,
-			default: 0,
-		},
+    articleLikes: {
+      type: Number,
+      default: 0,
+    },
 
-		articleViews: {
-			type: Number,
-			default: 0,
-		},
+    articleViews: {
+      type: Number,
+      default: 0,
+    },
 
-		articleComments: {
-			type: Number,
-			default: 0,
-		},
+    articleComments: {
+      type: Number,
+      default: 0,
+    },
 
-		memberId: {
-			type: Schema.Types.ObjectId,
-			required: true,
-			ref: 'Member',
-		},
-	},
-	{ timestamps: true, collection: 'boardArticles' },
+    memberId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Member',
+    },
+  },
+  { timestamps: true, collection: 'boardArticles' },
 );
 
 export default BoardArticleSchema;
